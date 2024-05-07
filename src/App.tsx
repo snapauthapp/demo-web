72ade96df261d9056b5ac6b9278bb7bbbc440e64
Eric Stern <eric@ericstern.com>
2024-05-07 15:10:53 -0700

    Add usage note

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,15 @@ function App() {
   return (
     <>
       <h1>SnapAuth Demo</h1>
+      <p>
+        This is a simple demo for <a href="https://www.snapauth.app?utm_source=Demo" target="_blank">SnapAuth</a>.
+
+        It does not store any local data, and "registering" here does <em>not</em> sign you up for SnapAuth (or anything else).
+        It's meant to give you an idea of what integration looks like and understand some of the API call flows.
+      </p>
+
+      <p>You can see the source on <a href="https://github.com/snapauthapp/demo-web" target="_blank">Github</a>.</p>
+
       <Tabs selectedTabId={selectedTabId} onChange={next => setSelectedTabId(next as TabIds)} renderActiveTabPanelOnly>
         <Tab id={TabIds.Register} title="Register" panel={<Register />} />
         <Tab id={TabIds.SignIn} title="Sign In" panel={<SignIn />} />
