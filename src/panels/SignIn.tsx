842c9b9e8a7db30ab22076b79dd6a4dfa796cd2a
Eric Stern <eric@ericstern.com>
2024-05-07 14:48:44 -0700

    Basic UI elements

--- src/panels/SignIn.tsx
+++ src/panels/SignIn.tsx
@@ -1,5 +1,37 @@
+import { useRef } from 'react'
+import { Button, FormGroup, InputGroup } from '@blueprintjs/core'
+
 const SignIn: React.FC = () => {
-  return <p>sign in panel</p>
+  const username = useRef<HTMLInputElement>(null)
+  const password = useRef<HTMLInputElement>(null)
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    alert(JSON.stringify({
+      username: username.current!.value,
+    }))
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <FormGroup label="Your username" labelInfo="(required)">
+        <InputGroup
+          autoComplete="username"
+          inputRef={username}
+          required
+        />
+      </FormGroup>
+      <FormGroup label="Your password" labelInfo="(required)">
+        <InputGroup
+          autoComplete="current-password"
+          inputRef={password}
+          required
+          type="password"
+        />
+      </FormGroup>
+      <Button type="submit" intent="primary">Sign In</Button>
+    </form>
+  )
 }
 
 export default SignIn
